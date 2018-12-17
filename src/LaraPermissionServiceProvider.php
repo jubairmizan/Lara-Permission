60a82536b4bfa91bfa5a0dec5d3350ffa66ddb3f
Jubair Mizan <jubairmizan@gmail.com>
2018-12-17 16:00:03 +0600

    Service Provider,Routes,View,Controllers and folder struct setup

---
+++ src/LaraPermissionServiceProvider.php
@@ -0,0 +1,31 @@
+<?php 
+namespace Jubairmizan\LaraPermission;
+use Illuminate\Support\ServiceProvider;
+
+class LaraPermissionServiceProvider extends ServiceProvider{
+	/**
+	 * Perform post-registration booting of services.
+	 */
+	public function boot(){
+		/**
+		 * If your package contains routes, you may load them using the loadRoutesFrom method. This method will automatically determine if the application's routes are cached and will not load your routes file if the routes have already been cached:
+		 */
+		$this->loadRoutesFrom(__DIR__.'/routes/web.php');
+		/**
+		 * To register your package's views with Laravel, you need to tell Laravel where the views are located. You may do this using the service provider's loadViewsFrom method. The loadViewsFrom method accepts two arguments: the path to your view templates and your package's name. For example, if your package's name is courier, you would add the following to your service provider's boot method:
+		 */
+		$this->loadViewsFrom(__DIR__.'/resources/views','lara-permission');
+		/**
+		 * If your package contains database migrations, you may use the loadMigrationsFrom method to inform Laravel how to load them. The loadMigrationsFrom method accepts the path to your package's migrations as its only argument:
+		 */
+		$this->loadMigrationsFrom(__DIR__.'/database/migrations');
+	}
+
+	/**
+	 * Perform post-registration booting of services.
+	 */
+	public function register(){
+
+	}
+}
+?>
---
+++ src/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+Route::get('role',function(){
+	return 'test';
+});
+?>
